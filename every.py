23abbdb644faa33cba268d924e0eed846ca04a72
mfrancis95 <mikefrancis95@gmail.com>
2016-01-02 12:15:06 -0500

    Added optional async argument. Uses ArgumentParser.

--- every.py
+++ every.py
@@ -1,25 +1,32 @@
-import sys
+from argparse import ArgumentParser
+import subprocess
 from time import sleep
-from subprocess import call
 
-if len(sys.argv) >= 3:
-    interval = sys.argv[1]
-    for i in range(len(interval)):
-        unit = interval[i]
-        if unit.isalpha():
-            if unit == "m":
-                unit = 60
-            elif unit == "h":
-                unit = 3600
-            elif unit == "d":
-                unit = 86400
-            else:
-                unit = 1
-            interval = float(interval[:i]) * unit
-            break
-    if type(interval) is str:
-        interval = float(interval)
-    process = " ".join(sys.argv[2:])
-    while True:
-        sleep(interval)
-        call(process, shell = True)
+parser = ArgumentParser("every")
+parser.add_argument("-a", action = "store_true")
+parser.add_argument("interval")
+parser.add_argument("command", nargs = "+")
+args = parser.parse_args()
+
+call = subprocess.Popen if args.a else subprocess.call
+interval = args.interval
+for i in range(len(interval)):
+    unit = interval[i]
+    if unit.isalpha():
+        if unit == "m":
+            unit = 60
+        elif unit == "h":
+            unit = 3600
+        elif unit == "d":
+            unit = 86400
+        else:
+            unit = 1
+        interval = float(interval[:i]) * unit
+        break
+if type(interval) is str:
+    interval = float(interval)
+process = " ".join(args.command)
+
+while True:
+    sleep(interval)
+    call(process, shell = True)
