714b2b5cb850cb07276d163991560dc3e94c7b57
mfrancis95 <mikefrancis95@gmail.com>
2016-01-22 21:09:20 -0500

    Code improvements

--- every.py
+++ every.py
@@ -8,25 +8,18 @@ parser.add_argument("interval")
 parser.add_argument("command", nargs = "+")
 args = parser.parse_args()
 
+units = {
+    "s": 1,
+    "m": 60,
+    "h": 3600,
+    "d": 86400
+}
+
 call = subprocess.Popen if args.async else subprocess.call
 interval = args.interval
-for i in range(len(interval)):
-    unit = interval[i]
-    if unit.isalpha():
-        if unit == "m":
-            unit = 60
-        elif unit == "h":
-            unit = 3600
-        elif unit == "d":
-            unit = 86400
-        else:
-            unit = 1
-        interval = float(interval[:i]) * unit
-        break
-if type(interval) is str:
-    interval = float(interval)
-process = " ".join(args.command)
+interval = float(interval[:-1]) * units[interval[-1]]
+command = " ".join(args.command)
 
 while True:
     sleep(interval)
-    call(process, shell = True)
+    call(command, shell = True)
