23ef2a7de7a1dd60bcc0704974c7397b4f914fed
mfrancis95 <mikefrancis95@gmail.com>
2016-02-15 21:28:43 -0500

    Add --start argument for specifying a starting time to run the interval, in %H:%M:%S format

--- every.py
+++ every.py
@@ -1,10 +1,11 @@
 from argparse import ArgumentParser
 import subprocess
-from time import sleep
+import time
 
 parser = ArgumentParser("every")
 parser.add_argument("-a", "--async", action = "store_true")
 parser.add_argument("interval")
+parser.add_argument("-s", "--start")
 parser.add_argument("command", nargs = "+")
 args = parser.parse_args()
 
@@ -18,8 +19,17 @@ units = {
 call = subprocess.Popen if args.async else subprocess.call
 interval = args.interval
 interval = float(interval[:-1]) * units[interval[-1]]
+start = args.start
 command = " ".join(args.command)
 
+if start:
+    now = time.localtime()
+    now = now[3] * 3600 + now[4] * 60 + now[5]
+    start = time.strptime(start, "%H:%M:%S")
+    start = start[3] * 3600 + start[4] * 60 + start[5] - now
+    if start > 0:
+        time.sleep(start)
+
 while True:
-    sleep(interval)
+    time.sleep(interval)
     call(command, shell = True)
