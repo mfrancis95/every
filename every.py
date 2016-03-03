c93bf13e6aecc6f2f7e911b0c6384dc7dad30968
mfrancis95 <mikefrancis95@gmail.com>
2016-03-03 09:30:14 -0500

    Use intervals module

--- every.py
+++ every.py
@@ -1,10 +1,11 @@
 from argparse import ArgumentParser
 import subprocess
 import time
+from intervals import Interval
 
 parser = ArgumentParser("every")
 parser.add_argument("-a", "--async", action = "store_true")
-parser.add_argument("interval")
+parser.add_argument("delay")
 parser.add_argument("-s", "--start")
 parser.add_argument("-t", "--times", default = -1, type = int)
 parser.add_argument("command", nargs = "+")
@@ -16,23 +17,15 @@ units = {
     "d": 86400
 }
 
-call = subprocess.Popen if args.async else subprocess.call
-interval = args.interval.strip()
-interval = float(interval[:-1]) * units.get(interval[-1], 1)
+call = lambda: (subprocess.Popen if args.async else subprocess.call)(" ".join(args.command), shell = True)
+delay = args.delay.strip()
+delay = float(delay[:-1]) * units.get(delay[-1], 1)
 start = args.start
-times = args.times
-command = " ".join(args.command)
 
 if start:
     now = time.localtime()
     now = now[3] * 3600 + now[4] * 60 + now[5]
     start = time.strptime(start, "%H:%M:%S")
     start = start[3] * 3600 + start[4] * 60 + start[5] - now
-    if start > 0:
-        time.sleep(start)
 
-while times != 0:
-    time.sleep(interval)
-    call(command, shell = True)
-    if times > 0:
-        times -= 1
+Interval(delay, call, start, args.times)
